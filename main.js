6014a756371b46422b72e533a083ff7160b6d043
yauacom <70412615+yauacom@users.noreply.github.com>
2021-06-10 20:51:45 -0700

    (completed) adding toggle menu function for mobile version

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 // /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkClicked = () => {
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((link) => link.addEventListener("click", linkClicked));
 
 /*==================== ACCORDION SKILLS ====================*/
 
